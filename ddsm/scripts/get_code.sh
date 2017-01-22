963ad3012ee4d15b4ed95a2e8fe2203f46b9aff4
Russ Ferriday <russ.ferriday@swga.co>
2017-01-22 11:45:37 +0000

    correct run dir

--- ddsm/scripts/get_code.sh
+++ ddsm/scripts/get_code.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/topiaruss/ddsm.git
+cd ddsm
 git checkout dockerise
 
--- ddsm/scripts/go.sh
+++ ddsm/scripts/go.sh
@@ -2,7 +2,7 @@
 
 ./get_code.sh
 
-cd ddsm/ddsm-software
+cd /ddsm/ddsm-software
 
 ./run-get-ddsm-mammo.sh
 
